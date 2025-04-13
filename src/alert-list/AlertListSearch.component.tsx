e5a13442707365512fd4aabe4473c722eb812a56
Ian Walton <ian@walton.dev>
2025-04-13 08:00:04 -0500

    make search easier to use

--- src/alert-list/AlertListSearch.component.tsx
+++ src/alert-list/AlertListSearch.component.tsx
@@ -1,6 +1,6 @@
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Button, Stack, TextField, Typography } from "@mui/material";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export type AlertListSearchProps = {
   onSearch: (newQuery: string) => void;
@@ -9,17 +9,31 @@ export type AlertListSearchProps = {
 const AlertListSearch = ({ onSearch }: AlertListSearchProps) => {
   const [newQuery, setNewQuery] = useState<string>("");
 
-  const handleSearch = () => onSearch(newQuery);
+  const handleSearch = useCallback<React.FormEventHandler<HTMLFormElement>>((evt) => {
+    onSearch(newQuery);
+    evt.preventDefault();
+  }, [onSearch, newQuery]);
 
   return (
-    <Stack direction="row" spacing={4}>
-      <TextField id="search" type="text" fullWidth placeholder="Search.." onBlur={(evt) => setNewQuery(evt.target.value)} />
+    <form onSubmit={handleSearch}>
+      <Stack direction="row" spacing={4}>
+        <TextField
+          id="search"
+          type="text"
+          fullWidth
+          label="Search.."
+          onChange={(evt) => setNewQuery(evt.target.value)}
+        />
 
-      <Button onClick={handleSearch} variant="contained" sx={{ width: "150px" }}>
-        <Typography>Search</Typography>
-        <SearchOutlinedIcon sx={{ marginLeft: 1 }} />
-      </Button>
-    </Stack>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ width: "150px" }}>
+            <Typography>Search</Typography>
+            <SearchOutlinedIcon sx={{ marginLeft: 1 }} />
+        </Button>
+      </Stack>
+    </form>
   )
 };
 
